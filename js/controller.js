df391707245cef2851cae79b8d12272a67270a5c
b00902130 <b00902130@ntu.edu.tw>
2014-06-30 15:30:47 +0800

    Now local storage has 3 groups, which named todoItems, finishedItems, deletedItems respectively.

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,37 @@
-function TodoCrtlUpdate($scope) {
+function TodoController($scope) {
   $scope.newItem = "";
   $scope.todoList = [];
+  // $scope.todoListArray = [];
+  $scope.finishedArray = [];
+  $scope.deletedArray = [];
   $scope.addItem = function(){
     if(this.newItem){
+      // this.todoListArray.push({label:this.newItem, isFinish:false, isDelete:false});
        this.todoList.push({label:this.newItem, isFinish:false, isDelete:false});
-       localStorage.todoItems = JSON.stringify(this.newItem);
+       localStorage.todoItems = JSON.stringify(this.todoList);
        this.newItem = "";
     }
   }
   $scope.finishItem = function(item){
       item.isFinish = true;
-      localStorage.finishedItems = JSON.stringify(item);
+      // item.isDelete = false;
+      localStorage.finishedItems = [];
+      localStorage.deletedItems = [];
+      this.finishedArray = [];
+      this.deletedArray = [];
+      for(var i=0; i<this.todoList.length; i++){
+        // if(item.label === this.todoListArray[i].label){
+        //   this.todoListArray[i].isFinish = true;
+        // }
+        if(this.todoList[i].isFinish === true && this.todoList[i].isDelete != true){
+          this.finishedArray.push(this.todoList[i]);
+        }
+        if(this.todoList[i].isDelete === true){
+          this.deletedArray.push(this.todoList[i]);
+        }
+      }
+      localStorage.finishedItems = JSON.stringify(this.finishedArray);
+      localStorage.deletedItems = JSON.stringify(this.deletedArray);
 
   }
   $scope.edit = function(item){
@@ -27,7 +48,32 @@ function TodoCrtlUpdate($scope) {
 
   $scope.deleteItem = function(item){
     item.isDelete = true;
-    localStorage.deletedItems = JSON.stringify(item);
+    // item.isFinish = false;
+    localStorage.finishedItems = [];
+    localStorage.deletedItems = [];
+    this.finishedArray = [];
+    this.deletedArray = [];
+    for(var i=0; i<this.todoList.length; i++){
+        // if(item.label === this.todoListArray[i].label){
+        //   this.todoListArray[i].isFinish = true;
+        // }
+        if(this.todoList[i].isFinish === true && this.todoList[i].isDelete != true){
+          this.finishedArray.push(this.todoList[i]);
+        }
+        if(this.todoList[i].isDelete === true){
+          this.deletedArray.push(this.todoList[i]);
+        }
+      }
+      localStorage.finishedItems = JSON.stringify(this.finishedArray);
+      localStorage.deletedItems = JSON.stringify(this.deletedArray);
 
   }
+  //   for(var i=0; i<this.todoList.length; i++){
+  //       if(this.todoList[i].isDelete === true){
+  //         this.deletedArray.push(this.todoList[i]);
+  //       }
+  //     }
+  //   localStorage.deletedItems = JSON.stringify(this.deletedArray);
+
+  // }
 }
